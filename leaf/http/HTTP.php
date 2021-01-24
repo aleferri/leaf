6af5c3c337a698f1828c2fa7d9d7cb021a052de0
Alessio Ferri <alessioferri@localhost.localdomain>
2021-01-24 17:54:05 +0100

    Add utilities function in HTTP and add LazyResponse class

--- leaf/http/HTTP.php
+++ leaf/http/HTTP.php
@@ -45,18 +45,85 @@ class HTTP {
         echo $r->body ();
     }
 
+    /**
+     * JSON response
+     * @param array $body content of json
+     * @param int $http_code code to send
+     * @param array $headers additional headers
+     * @return Response resulting response
+     */
     public static function json(array $body, int $http_code = 200, array $headers = []): Response {
         $headers[ 'Content-Type' ] = 'application/json';
 
         return new SimpleResponse ( $http_code, $headers, \json_encode ( $body ) );
     }
 
+    /**
+     * HTML response
+     * @param string $body body of the html
+     * @param int $http_code http code to send
+     * @param array $headers additional headers
+     * @return Response resulting response to send
+     */
     public static function html(string $body, int $http_code = 200, array $headers = []): Response {
         $headers[ 'Content-Type' ] = 'text/html';
 
         return new SimpleResponse ( $http_code, $headers, $body );
     }
     
+    /**
+     * XML response
+     * @param string $body body of the html
+     * @param int $http_code http code to send
+     * @param array $headers additional headers
+     * @return Response resulting response to send
+     */
+    public static function xml(string $body, int $http_code, array $headers = []): Response {
+        $headers[ 'Content-Type' ] = 'application/xml';
+
+        return new SimpleResponse ( $http_code, $headers, $body );
+    }
+    
+    /**
+     * Return file as a response
+     * @param string $body body of the file
+     * @param string $mime mime of the file
+     * @param string $filename filename to present for the user
+     * @param array $headers base headers, default empty, all required header will be overwritten
+     * @return Response resulting response to send
+     */
+    public static function file(string $body, string $mime, string $filename = 'document', array $headers = []): Response {
+        $headers[ 'Content-Control' ] = 'public';
+        $headers[ 'Content-Type' ] = "{$mime};";
+        $headers[ 'Content-Transfer-Encoding' ] = 'Binary';
+        $headers[ 'Content-Length' ] = strlen( $body );
+        $headers[ 'Content-Disposition' ] = "attachment; filename='{$filename}'";
+        return new SimpleResponse( 200, $headers, $body );
+    }
+    
+    /**
+     * Static file serve
+     * @param string $filename filename of the static file full path included
+     * @return Response resulting response to send
+     */
+    public static function static_file(string $filename): Response {
+        $basename = basename( $filename );
+        $mime = mime_content_type($filename);
+        $headers[ 'Content-Control' ] = 'public';
+        $headers[ 'Content-Type' ] = "{$mime};";
+        $headers[ 'Content-Transfer-Encoding' ] = 'Binary';
+        $headers[ 'Content-Length' ] = filesize( $filename );
+        $headers[ 'Content-Disposition' ] = "attachment; filename='{$basename}'";
+        return new LazyResponse( 200, $headers, function() use ($filename): string {
+            return file_get_contents($filename);
+        } );
+    }
+    
+    /**
+     * Redirect browser to another location
+     * @param string $location new location
+     * @return Response resulting response
+     */
     public static function redirect(string $location): Response {
         return new SimpleResponse ( 301, [ 'Location' => $location ], '' );
     }
---
+++ leaf/http/LazyResponse.php
@@ -0,0 +1,85 @@
+<?php
+
+/*
+ * Copyright 2021 alessioferri.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace leaf\http;
+
+/**
+ * LazyResponse delay body presentation until the last possible moment
+ *
+ * @author alessioferri
+ */
+class LazyResponse implements Response {
+    
+    private $http_code;
+    private $headers;
+    private $generator;
+    
+    public function __construct( int $http_code = 200, array $headers = [], ?callable $generator = null ) {
+        $this->http_code = $http_code;
+        $this->headers = $headers;
+        $this->generator = $generator;
+    }
+    
+    public function body(): string {
+        if ( $this->generator === null ) {
+            return '';
+        }
+        return ($this->generator)();
+    }
+
+    public function headers(): array {
+        return $this->headers;
+    }
+
+    public function status(): int {
+        return $this->http_code;
+    }
+
+    public function with_body(string $body): Response {
+        $this->generator = function() use ($body) {
+            return $body;
+        };
+        
+        return $this;
+    }
+
+    public function with_header(string $header, string $value): Response {
+        $this->headers[ $header ] = $value;
+        
+        return $this;
+    }
+
+    public function with_headers(array $headers): Response {
+        $this->headers = $headers;
+        
+        return $this;
+    }
+
+    public function with_status(int $http_code): Response {
+        $this->http_code = $http_code;
+        
+        return $this;
+    }
+
+    public function without_header(string $header): Response {
+        unset( $this->headers[ $header ] );
+        
+        return $this;
+    }
+
+}
